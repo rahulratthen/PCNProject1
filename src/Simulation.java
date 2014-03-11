3321a252ccd23636b2831de81cffccb08017aafb
rahulratthen <rahulratthen@yahoo.in>
2014-03-11 17:10:21 -0500

    Test commit

--- src/Simulation.java
+++ src/Simulation.java
@@ -8,6 +8,21 @@
  *
  * @author rahulratthen
  */
+
 public class Simulation {
+    public double m,k,seed, lambda=1;
     
+    public double GenerateCongruentialRV(){
+        seed = (k*seed)%m;
+        double random = seed/m;
+        return random;
+    }
+    
+    public double DistributedPseudoRandomNumber()
+    {
+        double y;
+        double rand = GenerateCongruentialRV();
+        y = -(1/lambda)*Math.log(rand);
+        return y;
+    }
 }
