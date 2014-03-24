999889ceff7cb5825c58e6f4e49dc08eddae7c94
rahulratthen <rahulratthen@yahoo.in>
2014-03-24 00:52:39 -0500

    First version
    Coded the sample provided

--- src/EventClass.java
+++ src/EventClass.java
@@ -1,3 +1,8 @@
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +13,39 @@
  *
  * @author rahulratthen
  */
-public class EventClass {
+public class EventClass  implements Comparable<EventClass>{
+    public String eventType;
+    public double time;
+    
+    public EventClass(String type, double t)
+    {
+        eventType = type;
+        time = t;
+    }
+
+    @Override
+    public int compareTo(EventClass o) {
+        if(this.time > o.time)
+            return 1;
+        else if(this.time == o.time)
+            return 0;
+        else
+            return -1;
+    }
+    
+//    public static void main(String args[])
+//    {
+//        LinkedList list = new LinkedList<EventClass>();
+//        
+//        list.add(new EventClass("ARR",1));
+//        list.add(new EventClass("DEP",2));
+//        list.add(new EventClass("ARR",5));
+//        list.add(new EventClass("ARR",3));
+//        EventClass.printList(list);
+//        
+//        Collections.sort(list);
+//        EventClass.printList(list);
+//    }
+//    
     
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,3 +1,8 @@
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,7 +15,81 @@
  */
 
 public class Simulation {
-    public double m,k,seed, lambda=1;
+    public double m,k,seed=1111.0, lambda1, lambda2, mu;
+    public int eventCount ;
+    LinkedList eventList;
+    public double clock, EN ;
+    public int N ,Ndep, Narr;
+    boolean done ;    
+    
+    public Simulation()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the value of \"m\"");
+        m = input.nextDouble();
+        System.out.println("Enter the value of \"k\"");
+        k = input.nextDouble();
+        System.out.println("Enter the value of \"lambda1\"");
+        lambda1 = input.nextDouble();
+        System.out.println("Enter the value of \"lambda2\"");
+        lambda2 = input.nextDouble();
+        System.out.println("Enter the value of \"mu\"");
+        mu = input.nextDouble();
+        
+        eventCount = 0;
+        eventList = new LinkedList<EventClass>();
+        clock = 0;
+        Ndep = 0;
+        Narr = 0;
+        EN = 0;
+        done = false;
+        
+    }
+    
+    public void Simulate()
+    {
+        //insert first ARR event
+        eventList.add(new EventClass("ARR",DistributedPseudoRandomNumber(lambda1)));
+        
+        while(!done)
+        {
+            EventClass currEvent = (EventClass)eventList.remove();
+            double prev = clock;
+            clock = currEvent.time;
+            
+            switch(currEvent.eventType)
+            {
+                case "ARR" :
+                    EN += N*(clock-prev);                 
+                    N++;
+                    Narr++;
+                    eventList.add(new EventClass("ARR",clock + DistributedPseudoRandomNumber(lambda1)));
+                    if(N==1)
+                    {
+                        eventList.add(new EventClass("DEP",clock + DistributedPseudoRandomNumber(mu)));
+                    }
+                    Collections.sort(eventList);
+                    break;
+                case "DEP" :
+                    EN += N*(clock-prev);  
+                    N--;
+                    Ndep++;
+                    if(N>0)
+                    {
+                        eventList.add(new EventClass("DEP",clock + DistributedPseudoRandomNumber(mu)));
+                    }
+                    Collections.sort(eventList);
+                    break;
+            }
+            
+            if(Ndep > 100000)
+                done = true;
+        }
+        
+        System.out.println("Current number of customers in the system : "+N);
+        //System.out.println("Expected number of customers(sim) : "+EN/clock);
+
+    }
     
     public double GenerateCongruentialRV(){
         seed = (k*seed)%m;
@@ -18,11 +97,16 @@ public class Simulation {
         return random;
     }
     
-    public double DistributedPseudoRandomNumber()
+    public double DistributedPseudoRandomNumber(double lambda)
     {
         double y;
         double rand = GenerateCongruentialRV();
         y = -(1/lambda)*Math.log(rand);
         return y;
+    }
+    
+    public static void main(String[] args) {
+        Simulation sim = new Simulation();
+        sim.Simulate();
     }
 }
